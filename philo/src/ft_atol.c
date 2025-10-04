03d57993c7c0eaff81f8093aab7b0e5f44a5ea8d
Kevin Bris <55791683+kebris-c@users.noreply.github.com>
2025-10-04 03:09:33 +0200

    Add files via upload

---
+++ philo/src/ft_atol.c
@@ -0,0 +1,23 @@
+#include "philo.h"
+
+long	ft_atol(const char *str)
+{
+	long	result;
+	int 	sign;
+	int 	i;
+
+    i = 0;
+    sign = 1;
+    result = 0;
+    while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+        i++;
+    if (str[i] == '-' || str[i] == '+')
+        if (str[i++] == '-')
+            sign = -sign;
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        result = result * 10 + (str[i] - '0');
+        i++;
+    }
+    return (result * sign);
+}
